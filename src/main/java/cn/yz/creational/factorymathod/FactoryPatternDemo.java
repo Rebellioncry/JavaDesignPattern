c609fc33bbcfd157cd3d6c8b5ec75ae827303caf
yz <709476884@qq.com>
2023-11-01 17:38:54 +0800

    更正工厂方法代码

--- src/main/java/cn/yz/creational/factorymathod/FactoryPatternDemo.java
+++ src/main/java/cn/yz/creational/factorymathod/FactoryPatternDemo.java
@@ -19,54 +19,47 @@ class Square implements Shape {
     }
 
 }
-class Circle implements Shape {
 
-    @Override
-    public void draw() {
-        System.out.println("Inside Circle::draw() method.");
-    }
-
-}
-class ShapeFactory {
+interface ShapeFactory {
 
     //使用 getShape 方法获取形状类型的对象
 
-    public Shape getShape(String shapeType){
-        if(shapeType == null){
-            return null;
-        }
-        if(shapeType.equalsIgnoreCase("CIRCLE")){
-            return new Circle();
-        } else if(shapeType.equalsIgnoreCase("RECTANGLE")){
-            return new Rectangle();
-        } else if(shapeType.equalsIgnoreCase("SQUARE")){
-            return new Square();
-        }
-        return null;
+    Shape getShape();
+}
+
+class RectangleFactory implements ShapeFactory{
+
+    @Override
+    public Shape getShape() {
+        return new Rectangle();
+    }
+}
+
+class SquareFactory implements ShapeFactory{
+
+    @Override
+    public Shape getShape() {
+        return new Square();
     }
 }
 
 public class FactoryPatternDemo {
     public static void main(String[] args) {
-        ShapeFactory shapeFactory = new ShapeFactory();
+        ShapeFactory rectangle = new RectangleFactory();
 
-        //获取 Circle 的对象，并调用它的 draw 方法
-        Shape shape1 = shapeFactory.getShape("CIRCLE");
+        //获取 rectangle 的对象，并调用它的 draw 方法
+        Shape shape1 = rectangle.getShape();
 
-        //调用 Circle 的 draw 方法
+        //调用 rectangle 的 draw 方法
         shape1.draw();
 
-        //获取 Rectangle 的对象，并调用它的 draw 方法
-        Shape shape2 = shapeFactory.getShape("RECTANGLE");
+        ShapeFactory square = new SquareFactory();
 
-        //调用 Rectangle 的 draw 方法
+        //获取 square 的对象，并调用它的 draw 方法
+        Shape shape2 = square.getShape();
+
+        //调用 square 的 draw 方法
         shape2.draw();
-
-        //获取 Square 的对象，并调用它的 draw 方法
-        Shape shape3 = shapeFactory.getShape("SQUARE");
-
-        //调用 Square 的 draw 方法
-        shape3.draw();
     }
 }
 
